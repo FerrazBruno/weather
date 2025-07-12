c548ec7c796a4b4265ce6f797cf3fc052fefa83c
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-07-12 16:28:58 -0300

    Weather Forecasting App

--- src/learn_cljs/weather.cljs
+++ src/learn_cljs/weather.cljs
@@ -1,39 +1,81 @@
 (ns ^:figwheel-hooks learn-cljs.weather
   (:require
-   [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]))
+    [ajax.core :as ajax]
+    [goog.dom :as gdom]
+    [reagent.core :as r]
+    [reagent.dom :as rdom]))
 
-(println "This text is printed from src/learn_cljs/weather.cljs. Go ahead and edit it and see reloading in action.")
+(defonce app-state
+  (r/atom {:title "WhichWeather"
+           :postal-code ""
+           :temperatures
+           {:today    {:label "Today"
+                       :value nil}
+            :tomorrow {:label "Tomorrow"
+                       :value nil}}}))
 
-(defn multiply [a b] (* a b))
+(def api-key "api-key->openweathermap.org")
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defn handle-response
+  [resp]
+  (let [today (get-in resp ["list" 0 "main" "temp"])
+        tomorrow (get-in resp ["list" 8 "main" "temp"])]
+    (swap! app-state
+           update-in [:temperatures :today :value] (constantly today))
+    (swap! app-state
+           update-in [:temperatures :tomorrow :value] (constantly tomorrow))))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(defn get-forecast!
+  []
+  (let [postal-code (:postal-code @app-state)]
+    (ajax/GET "http://api.openweathermap.org/data/2.5/forecast"
+              {:params {"id"    postal-code
+                        "appid" api-key
+                        "units" "imperial"}
+               :handler handle-response})))
 
-(defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/learn_cljs/weather.cljs and watch it change!"]])
+(defn title
+  []
+  [:h1 (:title @app-state)])
 
-(defn mount [el]
-  (rdom/render [hello-world] el))
+(defn temperature
+  [temp]
+  [:div {:class "temperature"}
+   [:div {:class "value"}
+    (:value temp)]
+   [:h2 (:label temp)]])
 
-(defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
+(defn go
+  []
+  [:button {:on-click get-forecast!} "Go"])
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
+(defn postal-code
+  []
+  [:div {:class "postal-code"}
+   [:h3 "Enter your postal code"]
+   [:input {:type "text"
+            :placeholder "Postal Code"
+            :value (:postal-code @app-state)
+            :on-change #(swap! app-state
+                               assoc :postal-code (-> % .-target .-value))}]
+   [go]])
+
+(defn app
+  []
+  [:div {:class "app"}
+   [title]
+   [:div {:class "temperatures"}
+    (for [temp (vals (:temperatures @app-state))]
+      [temperature temp])]
+   [postal-code]])
+
+(defn mount-app-element
+  []
+  (rdom/render [app] (gdom/getElement "app")))
+
 (mount-app-element)
 
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn ^:after-load on-reload
+  []
+  (mount-app-element))
+
